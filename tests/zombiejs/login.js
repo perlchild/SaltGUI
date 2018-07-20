1a9993dd64e1e9e502c9dd3a2192047aa6d3494e
Martijn Jacobs <martijn@devopsconsulting.nl>
2018-07-20 16:44:46 +0200

    Added some more tests

--- tests/zombiejs/login.js
+++ tests/zombiejs/login.js
@@ -12,8 +12,37 @@ describe('login page', function() {
       });
   });
 
-  it('we should have been redirected to the login page', function() {
-      browser.assert.url('http://localhost:3333/login')
-  });  
+  describe('redirects correctly', function() {
+
+    it('we should have been redirected to the login page', function() {
+        browser.assert.url('http://localhost:3333/login')
+    });
+
+  });
+
+  describe('submits loginform', function() {
+
+    before(function(done) {
+      browser
+        .fill('username', 'salt')
+        .fill('password', 'salt')
+        .pressButton('Login', function() {
+          done();
+      });
+    });
+
+    it('we should be logged in now', function() {
+      browser.assert.success();
+    });
+
+    it('and the Saltgui H1 tag is there', function() {
+      browser.assert.text('header h1', 'SaltGUI');
+    });
+
+    it('and a logout buttonis shown', function() {
+      browser.assert.element('header #button_logout');
+    });
+
+  });
 
 });
