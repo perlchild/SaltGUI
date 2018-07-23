528b79d1c26f2de09d20e998bab929d48870a714
Martijn Jacobs <martijn@devopsconsulting.nl>
2018-07-23 14:51:01 +0200

    Let's see if we can run the tests from a script

---
+++ runtests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# first see if we write es6 compatible js
+yarn lint
+
+# start a salt master, two salt minions and saltgui to run tests on
+docker-compose -f docker/docker-compose.yml up -d
+
+# wait until all are up
+yarn wait-for-docker
+
+# run the zombiejs tests
+yarn test
+
+# cleanup the docker images for 100% reproducable testresults
+ docker-compose -f docker/docker-compose.yml rm -f -s
